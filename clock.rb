16cb66f8ffaa35fb5132029daeb145e750dfa46a
Greg Burek <greg.burek@gmail.com>
2014-06-10 17:23:42 -0700

    Adds pgbench worker

--- clock.rb
+++ clock.rb
@@ -2,4 +2,11 @@ require "./lib/initializer"
 require "clockwork"
 
 module Clockwork
+  every(1.minute, "top-off-workers") do
+    PGPerf::PGBenchToolsWorker.top_off_workers
+  end
+
+  every(4.hours, "mark-restart") do
+    PGPerf::PGBenchToolsWorker.mark_restart
+  end
 end
---
+++ lib/workers/pgbench_tools_worker.rb
@@ -0,0 +1,114 @@
+require 'sidekiq'
+require 'open3'
+require_relative './hk_worker'
+
+module PGPerf
+  class PGBenchToolsWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: :pgbenchtools, retry: false
+
+    def self.mark_restart
+      queue = self.get_sidekiq_options["queue"].to_s
+      Sidekiq::ProcessSet.new.
+        select{|v| v['queues'].include?(queue)}.
+        each(&:stop!)
+    end
+
+    def self.top_off_workers
+      queue = self.get_sidekiq_options["queue"].to_s
+      PGPerf::HkWorker.perform_async(5, queue, "PX")
+    end
+
+    def perform(results_db, target_db, description, script_name)
+      results_db_uri = URI.parse(results_db)
+      target_db_uri = URI.parse(target_db)
+      env = pgbench_env(results_db_uri, target_db_uri, script_name)
+      write_pgpass_file(results_db_uri, target_db_uri)
+      run("./newset '#{description}'", env, chdir: '/app/vendor/pgbench-collector')
+      run("./runset", env, chdir: '/app/vendor/pgbench-collector')
+    end
+
+    def run(command, env={}, opts={})
+      logger.info "running #{command}"
+      result = nil
+      Open3.popen3(env, *command, opts) do |stdin, stdout, stderr, wait_thr|
+        stdin.close
+        err_thr = Thread.new { stderr.each_line { |l| logger.info("stderr: #{l.chomp}") } }
+        puts "Reading STDOUT"
+        stdout.each_line { |l| logger.info("stdout: #{l.chomp}") }
+        err_thr.join
+
+        result = wait_thr.value.exitstatus
+      end
+    ensure
+      logger.info "#{command} completed with exit status #{result}"
+    end
+
+    def pgpass_filename
+      '/app/.pgpass'
+    end
+
+    def write_pgpass_file(rdb_uri, tdb_uri)
+      File.open(pgpass_filename, 'a') do |f|
+        f.puts "%s:%s:%s:%s:%s" % [rdb_uri.host,
+                                   rdb_uri.port,
+                                   rdb_uri.path.gsub('/',''),
+                                   rdb_uri.user,
+                                   rdb_uri.password]
+        f.puts "%s:%s:%s:%s:%s" % [tdb_uri.host,
+                                   tdb_uri.port,
+                                   tdb_uri.path.gsub('/',''),
+                                   tdb_uri.user,
+                                   tdb_uri.password]
+      end
+      File.chmod(0600, pgpass_filename)
+    end
+
+    def pgbench_env(rdb_uri, tdb_uri, script_name)
+      px_config.merge(
+        target_db_config(tdb_uri)
+      ).merge(
+        results_db_config(rdb_uri)
+      ).merge(
+        {
+          'SCRIPT' => "%s.sql" % script_name,
+        }
+      )
+    end
+
+
+    def target_db_config(db_uri)
+      {
+      'TESTHOST'    => db_uri.host,
+      'TESTUSER'    => db_uri.user,
+      'TESTPORT'    => db_uri.port.to_s,
+      'TESTDB'      => db_uri.path.gsub('/',''),
+      }
+    end
+
+    def results_db_config(db_uri)
+      {
+      'RESULTHOST'  => db_uri.host,
+      'RESULTUSER'  => db_uri.user,
+      'RESULTPORT'  => db_uri.port.to_s,
+      'RESULTDB'    => db_uri.path.gsub('/',''),
+      }
+    end
+
+    def px_config
+      {
+      'SKIPINIT'    => '0',
+      'SKIPREPORT'  => '1',
+      'TABBED'      => '0',
+      'OSDATA'      => '0',
+      'MAX_WORKERS' => '8',
+      'SCALES'      => '750',
+      'SETCLIENTS'  => '1 10 50 100 150 200 250 300',
+      'SETTIMES'    => '1',
+      'RUNTIME'     => '600',
+      'PGPASSFILE'  => pgpass_filename,
+      }
+    end
+  end
+end
+
