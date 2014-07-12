620afd158f3b76808fd01b0f8bd464a6035fad4d
Greg Burek <greg.burek@gmail.com>
2014-07-12 15:27:19 -0700

    Adds sequel json extension for json datatypes

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,1 +1,3 @@
-Sequel.connect(Config.database_url, max_connections: Config.db_pool)
+Sequel.extension :pg_array, :pg_array_ops
+DB = Sequel.connect(Config.database_url, max_connections: Config.db_pool)
+DB.extension :pg_array, :pg_json
