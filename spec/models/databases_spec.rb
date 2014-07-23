9f02db29ceb4f19b24cfb9702711a6c687d6a3b4
Timothée Peignier <timothee.peignier@tryphon.org>
2014-07-23 11:14:46 -0700

    fix spec

--- spec/models/databases_spec.rb
+++ spec/models/databases_spec.rb
@@ -8,6 +8,8 @@ describe Database do
   it "enqueues a benchmark" do
     expect(PGPerf::PGBenchToolsWorker).to receive(:perform_async).with(Config.database_url,
       database.admin_url, database.description, "select")
+    expect(PGPerf::PGBenchToolsWorker).to receive(:perform_async).with(Config.database_url,
+    database.admin_url, database.description, "tpc-b")
     database.enqueue_benchmark
   end
 end
