8d582935f4e06347960585d79eeadd582de5429c
Greg Burek <greg.burek@gmail.com>
2014-07-31 17:08:25 -0700

    Change testset migration to alter table not create it
    The testset migration should not create the lone testset table, but it
    should alter the one created by resultsdb.sql, at least until all of
    resultsdb.sql is included in the migrations.

--- db/migrate/1406149630_create_test_set.rb
+++ db/migrate/1406149630_create_test_set.rb
@@ -1,14 +1,7 @@
 Sequel.migration do
   change do
-    create_table(:testset) do
-      serial :set, primary_key: true
-      text :info, unique: true, null: false
-      text :testdb, null: false
-      text :testuser, null: false
-      text :testhost, null: false
-      text :testport, null: false
-      text :settings
-      foreign_key :database_uuid, :databases, type: :uuid
+    alter_table(:testset) do
+      add_foreign_key :database_uuid, :databases, type: :uuid
     end
   end
 end
