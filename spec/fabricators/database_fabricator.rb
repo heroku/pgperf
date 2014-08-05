25a6c0b5efa48167c1fae75cf0e9f526d6483cbd
Greg Burek <greg.burek@gmail.com>
2014-08-04 19:00:09 -0700

    Add required port

--- spec/fabricators/database_fabricator.rb
+++ spec/fabricators/database_fabricator.rb
@@ -1,8 +1,8 @@
 Fabricator(:database, from: Database) do
   uuid { SecureRandom.uuid }
   shogun_name "shogun-perf"
-  resource_url "postgres://user:password@localhost/perf"
-  admin_url "postgres://admin:password@localhost/perf"
+  resource_url "postgres://user:password@localhost:5432/perf"
+  admin_url "postgres://admin:password@localhost:5432/perf"
   heroku_id { sequence(:heroku_id) { |s| "resource#{s}@herokutest.com" } }
   plan "standard-ika"
   app "perf"
